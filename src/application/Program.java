983504fe4dcd60e297b814a8e0a3ccad746dae77
gscode <adm.guilhermedesousa@gmail.com>
2024-01-03 13:00:09 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,16 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+       // Department obj = new Department(1, "Books");
 
-        Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
+       // Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); // Aqui foi instanciado o DAO sem a necessidade do new, devido ser uma interface.
-        
-        System.out.println(seller);
+
+        Seller seller1 = sellerDao.findById(3);
+
+        // System.out.println(seller);
+         System.out.println(seller1);
+
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJBDC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() { // Expondo o método (createSellerDao) que retorna o tipo da interface (SellerDao).
-        return new SellerDaoJBDC(); // Mas internamente irá instanciar uma implementação.
+        return new SellerDaoJBDC(DB.getConnection()); // Mas internamente irá instanciar uma implementação.
     }
 }
